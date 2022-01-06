ceedd029cba641e19731f559159ca2430d6455c3
hyhello <673089899@qq.com>
2022-01-06 17:24:29 +0800

    fix: 修改细节

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,94 @@
-declare namespace Utils {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
+type ArgType = string | number | { [propName: string]: unknown };
+
+interface IFunction {
+	(arg1: ArgType, arg2: ArgType): number;
+}
+
+type IParams = {
+	[propName: string]: unknown;
+};
+
+interface IURL {
+	source: string;
+	protocol: string;
+	host: string;
+	port: string;
+	origin: string;
+	pathname: string;
+	query: string;
+	params: IParams;
+	path: string;
+	hash: string;
+	file: string;
+	relative: string;
+	segments: {
+		[index: number]: string;
+	};
+}
+declare namespace utils {
 	export function chunk<T>(input: T[], size?: number): T[] | T[][];
 	export function compact<T>(input: T[]): T[];
+	export function compare(key?: string | number): IFunction;
+	export function oneOf<T>(target: T, list: Array<T>): boolean;
+	export function rangeArr<T>(n: number, iteratee?: T | undefined): Array<T | undefined>;
+	export function toArray<T>(likeArr: ArrayLike<T>): Array<T>;
+	export function unique<T>(arr: Array<T>): Array<T>;
+	export function get(obj: object, path: string | Array<string | number>, defaultValue?: unknown): any;
+	export function getStore(name: string, deep?: boolean): string | null;
+	export function getType(v: unknown): string;
+	export function isArray(v: unknown): boolean;
+	export function isBoolean(v: unknown): boolean;
+	export function isElement(el: unknown): boolean;
+	export function isEmpty(val: unknown): boolean;
+	export function isFunction(input: unknown): boolean;
+	export function isInteger(n: unknown): boolean;
+	export function isNative(ctor: Function): boolean;
+	export function isNumber(v: unknown): boolean;
+	export function isObject(arg: unknown): boolean;
+	export function isPlainObject(v: unknown): boolean;
+	export function isPrimitive(v?: unknown): boolean;
+	export function isPromise(input: unknown): boolean;
+	export function isUndefined(v?: unknown): boolean;
+	export function removeStore(name: string, deep?: boolean): void;
+	export function setStore(name: string, value: string | unknown, deep?: boolean): void;
+	export function uuid(): string;
+	export function formatDate(date: number | string | Date, fmt = 'yyyy-MM-dd hh:mm:ss'): string;
+	export function isLeapYear(year: number): boolean;
+	export function addClass(el: HTMLElement, cls: string): void;
+	export function hasClass(el: HTMLElement, cls: string): boolean;
+	export function maybeAddPx(input: string | number): string;
+	export function off(el: HTMLElement, type: string, listener: EventListenerOrEventListenerObject): void;
+	export function offset(el: HTMLElement): { x: number; y: number };
+	export function on(el: HTMLElement, type: string, listener: EventListenerOrEventListenerObject): void;
+	export function once(el: HTMLElement, type: string, listener: EventListener): void;
+	export function removeClass(el: HTMLElement, cls: string): void;
+	export function scrollTo(el: HTMLElement, end = 0, duration = 500): Promise<void>;
+	export function toggleClass(el: HTMLElement, cls: string): void;
+	export function noop(): void;
+	export function max(arr: Array<number>): number;
+	export function min(arr: Array<number>): number;
+	export function random(minVal: number, maxVal: number): number;
+	export function toFixed(num: number, fixed?: number): string;
+	export function isChinese(str: string): boolean;
+	export function isEmail(str: string): boolean;
+	export function isIdCard(str: string | number): boolean;
+	export function isTel(str: number | string): boolean;
+	export function isUrl(str: string): boolean;
+	export function camelCase(str: string, capital?: boolean): string;
+	export function capitalize(str: string): string;
+	export function ltrim(input: string): string;
+	export function parseUrl(url: string): IURL;
+	export function pathResolve(basePath: string, ...args: string[]): string;
+	export function rtrim(input: string): string;
+	export function split(val: number | string, index = 3, separated = ','): string;
+	export function template(str: string, opt: object): string;
+	export function toNumber(str: string): number;
+	export function trim(input: string): string;
+	export function uncapitalize(str: string): string;
+}
+
+declare module 'utils' {
+	export = utils;
 }
