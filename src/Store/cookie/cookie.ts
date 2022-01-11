1fb905cf60cc85109b82095061d66ad5dfd19e02
hyhello <673089899@qq.com>
2022-01-11 11:01:18 +0800

    fix: 编码cookie

--- src/Store/cookie/cookie.ts
+++ src/Store/cookie/cookie.ts
@@ -15,11 +15,10 @@ const cookie: ICookie = {
 		name = window.decodeURIComponent(name);
 		const match = new RegExp(`\\b${name}=([^;]*)\\b`).exec(document.cookie);
 		if (!match) return null;
-		return window.unescape(match[1]);
+		return match[1];
 	},
 	set(name: string, value: string, day = 30): void {
 		name = window.encodeURIComponent(name);
-		value = window.escape(value);
 		document.cookie = `${name}=${value}; expires=${new Date().getTime() + day * 24 * 60 * 60 * 1000}; path=/;`;
 	},
 	clear(name: string): void {
