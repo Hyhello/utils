928f9dceb2f3c0b3f05d0126fd9c2debe299635c
hyhello <673089899@qq.com>
2022-01-11 11:02:18 +0800

    fix: 修改cookie编码

--- src/Store/cookie/cookie.ts
+++ src/Store/cookie/cookie.ts
@@ -15,10 +15,11 @@ const cookie: ICookie = {
 		name = window.decodeURIComponent(name);
 		const match = new RegExp(`\\b${name}=([^;]*)\\b`).exec(document.cookie);
 		if (!match) return null;
-		return match[1];
+		return window.decodeURIComponent(match[1]);
 	},
 	set(name: string, value: string, day = 30): void {
 		name = window.encodeURIComponent(name);
+		value = window.encodeURIComponent(value);
 		document.cookie = `${name}=${value}; expires=${new Date().getTime() + day * 24 * 60 * 60 * 1000}; path=/;`;
 	},
 	clear(name: string): void {
