f4d3f04fa2c742a24414b82160dbabf519ccc333
hyhello <673089899@qq.com>
2022-01-17 18:08:59 +0800

    fix: 修复scrollTo 导致node环境引入包报错

--- src/Dom/scrollTo/scrollTo.ts
+++ src/Dom/scrollTo/scrollTo.ts
@@ -1,8 +1,6 @@
 const cubic = (value: number): number => value ** 3; // 类似于 Math.pow(value, 3);
 const easeInOutCubic = (value: number): number => (value < 0.5 ? cubic(value * 2) / 2 : 1 - cubic((1 - value) * 2) / 2);
 
-const rAF = window.requestAnimationFrame || ((func) => setTimeout(func, 16));
-
 export default function scrollTo(el: HTMLElement, end = 0, duration = 500): Promise<void> {
 	return new Promise((resolve) => {
 		const beginTime = Date.now();
@@ -12,12 +10,12 @@ export default function scrollTo(el: HTMLElement, end = 0, duration = 500): Prom
 			const progress = (Date.now() - beginTime) / duration;
 			if (progress < 1) {
 				el.scrollTop = beginValue - easeInOutCubic(progress) * diff;
-				rAF(frameFunc);
+				window.requestAnimationFrame(frameFunc);
 			} else {
 				el.scrollTop = end;
 				resolve();
 			}
 		};
-		rAF(frameFunc);
+		window.requestAnimationFrame(frameFunc);
 	});
 }
