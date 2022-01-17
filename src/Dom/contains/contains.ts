e471efc67c4b2e66338fc4722fa5ead74424f923
hyhello <673089899@qq.com>
2022-01-17 16:20:30 +0800

    feat: 更新types及deepClone、get方法；修复contains，hasFocus

--- src/Dom/contains/contains.ts
+++ src/Dom/contains/contains.ts
@@ -4,13 +4,12 @@
  * @example
  *
  */
-export default function contains(root: HTMLElement, el: HTMLElement): boolean {
+export default function contains(root: Node, el: Node): boolean {
+	if (root === el) return true;
+	if (root.contains) return root.contains(el);
 	// eslint-disable-next-line no-bitwise
-	if (root.compareDocumentPosition) return root === el || !!(root.compareDocumentPosition(el) & 16);
-	if (root.contains && el.nodeType === 1) {
-		return root.contains(el) && root !== el;
-	}
+	if (root.compareDocumentPosition) return !!(root.compareDocumentPosition(el) & 16);
 	// eslint-disable-next-line no-cond-assign
-	while ((el = <HTMLElement>el.parentNode)) if (el === root) return true;
+	while ((el = <Node>el.parentNode)) if (el === root) return true;
 	return false;
 }
