df27bdb32e1ba75a3fd42eeb0daa9ee7e493a227
hyhello <673089899@qq.com>
2022-03-09 16:16:32 +0800

    🐞 fix: 修改pathResolve绝对路径拼接问题

--- src/String/pathResolve/pathResolve.ts
+++ src/String/pathResolve/pathResolve.ts
@@ -4,15 +4,29 @@
  * @param args
  * @returns path
  * @example
- * pathResolve('/b', '/ad/bc', './c', './a/dddd'); => '/b/ad/bc/c/a/dddd'
+ * pathResolve('/b', '/ad/bc', './c', './a/dddd'); => '/ad/bc/c/a/dddd'
  * pathResolve('/b', '/ad/bc', '../c'); => '/b/ad/c'
  */
-export default function pathResolve(basePath: string, ...args: string[]): string {
-	let path: string[] = basePath.replace(/\/$/, '').split('/');
-	args.forEach((arg) => {
-		const pathList: string[] = arg.split('/').filter((o) => o && o !== '.');
-		const len: number = pathList.filter((o) => o === '..').length;
-		path = path.slice(0, path.length - len).concat(pathList.slice(len));
-	});
-	return path.join('/');
+export default function pathResolve(...args: string[]): string {
+	// 分隔符
+	let path = '';
+	for (let i = args.length - 1; i >= 0; i--) {
+		const url = args[i];
+		path = `${url}/${path}`;
+		if (/^\//.test(url)) break;
+	}
+	const pathList: string[] = path.split('/').reduce((arr: string[], item, index) => {
+		if (item === '..') {
+			arr = arr.slice(0, -1);
+			if (arr.length === 0) {
+				// 此处主要是解决../ 转为绝对路径
+				arr.push('');
+			}
+		} else if ((!index || item) && item !== '.') {
+			// 此处判定index，主要是作用于是否是absolutePath
+			arr.push(item);
+		}
+		return arr;
+	}, []);
+	return pathList.join('/');
 }
