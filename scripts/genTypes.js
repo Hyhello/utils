f2a2c2ce8639c548723e377e893ce308ead02fb1
hyhello <673089899@qq.com>
2022-06-25 10:03:46 +0800

    chore: 修改构建脚本, 添加自动生成.d.ts脚本文件

--- scripts/genTypes.js
+++ scripts/genTypes.js
@@ -7,11 +7,17 @@ const config = require('./config');
 const outputPath = path.resolve(config.outputDir);
 
 // 生成{name}.d.ts
-const genDebrisTypes = () => {
+const genTypes = () => {
     const options = {
         "declaration": true,
         "emitDeclarationOnly": true
     };
+
+    const importList = [];
+    const declareList = [];
+    const exportList = [];
+
+    const simpleReg = /^(\w+).d.ts$/;
 
     const components = glob.sync(['./src/**/*.ts'], {
         ignore: ['./src/**/index.ts', './src/**/*.spec.ts']
@@ -20,37 +26,24 @@ const genDebrisTypes = () => {
     const host = ts.createCompilerHost(options)
 
     host.writeFile = (fileName, contents) => {
-        fs.writeFileSync(path.join(outputPath, path.basename(fileName)), contents, 'utf-8');
+        const basename = path.basename(fileName);
+        const name = basename.replace(simpleReg, '$1');
+        importList.push(`import ${name} from './${name}';`);
+        declareList.push(`\t${name}: typeof ${name}`);
+        exportList.push(`export { default as ${name} } from './${name}';`);
+        fs.writeFileSync(path.join(outputPath, basename), contents, 'utf-8');
     }
 
     const compiler = ts.createProgram(components, options, host);
 
     compiler.emit();
-};
 
-// 生成index.d.ts
-const genIndexTypes = () => {
-    const simpleReg = /^(\w+).d.ts$/;
-
-    const dList = glob.sync(['*.d.ts'], {
-        cwd: outputPath
-    });
-    const importList = [];
-    const mainList = [];
-    const exportList = [];
-    dList.forEach(fileName => {
-        const name = fileName.replace(simpleReg, '$1');
-        importList.push(`import ${name} from './${name}';`);
-        mainList.push(`\t${name}: typeof ${name}`);
-        exportList.push(`export { default as ${name} } from './${name}';`);
-    });
-    const contentList = importList.concat([`declare const ${ config.pkgName }: {\n${mainList.join(';\n')};\n}`, `export default ${ config.pkgName }`], exportList);
+    // 生成index.d.ts
+    const contentList = importList.concat(
+        [`\ndeclare const ${config.pkgName}: {\n${declareList.join(';\n')};\n}`, `export default ${config.pkgName};\n`],
+        exportList
+    );
     fs.writeFileSync(path.join(outputPath, 'index.d.ts'), contentList.join('\n'), 'utf-8');
-};
-
-const genTypes = () => {
-    genDebrisTypes();
-    genIndexTypes();
 };
 
 module.exports = genTypes;
