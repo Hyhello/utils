23bdc80c4f4b7c677733fabd009d0199acb95433
hyhello <673089899@qq.com>
2022-06-28 14:35:50 +0800

    docs: 修改注释commont

--- src/Dom/once/once.ts
+++ src/Dom/once/once.ts
@@ -7,7 +7,7 @@ import { isBrowser } from '@/Base';
  * @param el 待处理的 HTML 元素。
  * @param type 表示监听事件类型的字符串。
  * @param listener 当所监听的事件类型触发时，会接收到一个事件通知的函数。
- * @see {@link https://hyhello.github.io/utils/#/maybeAddPx 在线文档}
+ * @see {@link https://hyhello.github.io/utils/#/once 在线文档}
  */
 export default function once(el: Element, type: string, listener: EventListener): void {
 	if (!isBrowser()) throw new Error('This method is not supported in the current environment');
