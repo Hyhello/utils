e421196ab2092e1bfa972970abe872d44d281523
hyhello <673089899@qq.com>
2022-08-18 15:30:21 +0800

    feat: Function 分类新增 curry 方法

--- src/Function/index.ts
+++ src/Function/index.ts
@@ -3,3 +3,4 @@ export { default as after } from './after/after';
 export { default as before } from './before/before';
 export { default as throttle } from './throttle/throttle';
 export { default as debounce } from './debounce/debounce';
+export { default as curry } from './curry/curry';
