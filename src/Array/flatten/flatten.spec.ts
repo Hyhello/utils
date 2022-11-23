0afde1403343dd2b94e43c7b49c6ce6c658be7a6
Hyhello <673089899@qq.com>
2022-11-24 00:08:54 +0800

    docs: 修改README.md

--- src/Array/flatten/flatten.spec.ts
+++ src/Array/flatten/flatten.spec.ts
@@ -3,7 +3,6 @@ import flatten from './flatten';
 describe('#flatten()', () => {
 	const list = [[1, [2, [4]], [3]]];
 	test('flatten test', () => {
-		console.log(JSON.stringify(flatten(list, 1)));
 		expect(flatten(list, 0)).toEqual([[1, [2, [4]], [3]]]);
 		expect(flatten(list, -1)).toEqual([[1, [2, [4]], [3]]]);
 		expect(flatten(list)).toEqual([1, [2, [4]], [3]]);
