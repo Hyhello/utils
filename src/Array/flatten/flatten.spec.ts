0dd3e1c97759d135010dcce1d190749dac52309a
Hyhello <673089899@qq.com>
2022-11-23 20:14:13 +0800

    fix: 分类Array新增flatten扁平化函数添加depth参数

--- src/Array/flatten/flatten.spec.ts
+++ src/Array/flatten/flatten.spec.ts
@@ -3,6 +3,13 @@ import flatten from './flatten';
 describe('#flatten()', () => {
 	const list = [[1, [2, [4]], [3]]];
 	test('flatten test', () => {
-		expect(flatten(list)).toEqual([1, 2, 4, 3]);
+		console.log(JSON.stringify(flatten(list, 1)));
+		expect(flatten(list, 0)).toEqual([[1, [2, [4]], [3]]]);
+		expect(flatten(list, -1)).toEqual([[1, [2, [4]], [3]]]);
+		expect(flatten(list)).toEqual([1, [2, [4]], [3]]);
+		expect(flatten(list, true)).toEqual([1, 2, 4, 3]);
+		expect(flatten(list, 1)).toEqual([1, [2, [4]], [3]]);
+		expect(flatten(list, 2)).toEqual([1, 2, [4], 3]);
+		expect(flatten(list, 3)).toEqual([1, 2, 4, 3]);
 	});
 });
--- src/RegExp/isMobile/isMobile.ts
+++ src/RegExp/isMobile/isMobile.ts
@@ -4,6 +4,6 @@
  * @returns {boolean} 如果 str 是手机号码，那么返回 true，否则返回 false。
  * @see {@link https://hyhello.github.io/utils/#/isMobile 在线文档}
  */
-export default function isTel(str: number | string): boolean {
+export default function isMobile(str: number | string): boolean {
 	return /^(\+?0?86-?)?1[3456789]\d{9}$/.test(String(str));
 }
