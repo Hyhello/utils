e25be1c318c90029e64b53de51a69dfa375d7ad6
Hyhello <673089899@qq.com>
2022-11-24 23:21:54 +0800

    feat: 分类Math新增inRange方法。

---
+++ src/Math/inRange/inRange.spec.ts
@@ -0,0 +1,13 @@
+import inRange from './inRange';
+
+describe('#inRange()', () => {
+	test('inRange test', () => {
+		expect(inRange(3, 2, 4)).toBeTruthy();
+		expect(inRange(4, 2, 4)).toBeFalsy();
+		expect(inRange(4, 8)).toBeTruthy();
+		expect(inRange(4, 2)).toBeFalsy();
+		expect(inRange(-3, -2, -3)).toBeTruthy();
+		expect(inRange(-2, -2, -3)).toBeFalsy();
+		expect(inRange(-1, -2, -3)).toBeFalsy();
+	});
+});
