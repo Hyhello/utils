9b8e4e7f51b29c4a8ea3c100b46b1dc8af3d3784
Hyhello <673089899@qq.com>
2022-11-26 13:33:50 +0800

    fix: 修复split函数浮点数格式化达不到预期问题。

--- src/String/split/split.spec.ts
+++ src/String/split/split.spec.ts
@@ -5,6 +5,7 @@ describe('#split()', () => {
 		expect(split('')).toEqual('');
 		expect(split('aaaa')).toEqual('a,aaa');
 		expect(split('bbbbb', 2)).toEqual('b,bb,bb');
+		expect(split('566.321', 2)).toEqual('5,66.321');
 		expect(split('ccccc', 2, '|')).toEqual('c|cc|cc');
 	});
 });
