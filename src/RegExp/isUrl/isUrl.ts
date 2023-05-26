ad4088a269268f7ff81f11fa65997bae01d13422
Hyhello <673089899@qq.com>
2023-05-26 18:20:36 +0800

    fix: 优化isUrl正则表达式

--- src/RegExp/isUrl/isUrl.ts
+++ src/RegExp/isUrl/isUrl.ts
@@ -5,7 +5,5 @@
  * @see {@link https://hyhello.github.io/utils/#/isUrl 在线文档}
  */
 export default function isUrl(str: string): boolean {
-	return /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/.test(
-		str
-	);
+	return /^(((ht|f)tps?):\/\/)?([^!@#$%^&*?.\s-]([^!@#$%^&*?.\s]{0,63}[^!@#$%^&*?.\s])?\.)+[a-z]{2,6}\/?/.test(str);
 }
