e736a628576fc990e8a406436f0cad295f5ea462
Hyhello <673089899@qq.com>
2024-05-07 09:32:26 +0800

    fix: 调整isFunction判定，去掉不必要的判定

--- src/Base/isFunction/isFunction.ts
+++ src/Base/isFunction/isFunction.ts
@@ -1,14 +1,10 @@
-type IFunction = {
-	(input: unknown): boolean;
-	nodeType?: unknown;
-};
-
 /**
  * 检测变量（v）是否是一个函数。
- * @param input 待检测类型的变量。
+ * @param v 待检测类型的变量。
  * @returns {boolean} 一个指示指定的变量是否为 函数。
  * @see {@link https://hyhello.github.io/utils/#/isFunction 在线文档}
  */
-export default function isFunction(input: unknown): boolean {
-	return typeof input === 'function' && typeof (<IFunction>input).nodeType !== 'number';
+// eslint-disable-next-line @typescript-eslint/ban-types
+export default function isFunction(v: unknown): v is Function {
+    return typeof v === 'function';
 }
