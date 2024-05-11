eec522272ba21886a4258e1d1fc12e35d69f49f8
Hyhello <673089899@qq.com>
2024-05-11 18:28:06 +0800

    test: 完善set.spec.ts

--- src/Base/set/set.spec.ts
+++ src/Base/set/set.spec.ts
@@ -5,7 +5,7 @@ describe('#set()', () => {
         const obj1 = { a: [{ b: { c: 6 } }] };
         const obj2 = {};
         set(obj1, 'a[0].b.c', '默认值');
-        set(obj2, 'a.0.b.c', '默认值');
+        set(obj2, ['a', '0', 'b', 'c'], '默认值');
         expect(obj1).toEqual(obj2);
         set(obj1, 'a[0].b[0]', 2);
         expect(obj1).toEqual({ a: [{ b: { 0: 2, c: '默认值' } }] });
