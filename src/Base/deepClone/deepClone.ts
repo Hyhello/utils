d95d133b165ec10e3e7d7f37651ec0c5ad511bd2
Hyhello <673089899@qq.com>
2024-05-11 17:04:26 +0800

    feat: deepClone 添加对Set、Map、WeakMap 深度复制

--- src/Base/deepClone/deepClone.ts
+++ src/Base/deepClone/deepClone.ts
@@ -1,25 +1,47 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import { isObject, isArray } from '@/Base';
+
 const hasOwn = Object.prototype.hasOwnProperty;
+
 /**
  * 对象深拷贝。
- * @param data 待拷贝的值。
- * @returns {any} 深拷贝后的值。
+ * @param v 待拷贝的值。
+ * @returns {T} 深拷贝后的值。
  * @see {@link https://hyhello.github.io/utils/#/deepClone 在线文档}
  */
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default function deepClone(data: any): any {
-    let result: any;
-    if (data === null || typeof data !== 'object') return data;
-    if (data instanceof Date) {
-        result = new Date();
-        result.setTime(data.getTime());
-    } else if (typeof data === 'object') {
-        result = data instanceof Array ? [] : {};
-        for (const key in data) {
-            if (hasOwn.call(data, key)) {
-                result[key] = deepClone(data[key]);
-            }
+export default function deepClone<T>(v: T): T {
+    if (!isObject(v)) return v;
+
+    if (v instanceof WeakMap) {
+        throw new Error('Cannot clone WeakMap: WeakMap keys must be objects');
+    }
+
+    if (v instanceof Date) {
+        return new Date(v.getTime()) as any as T;
+    }
+
+    if (v instanceof Set) {
+        const clonedSet = new Set<unknown>();
+        v.forEach((value) => {
+            clonedSet.add(deepClone(value));
+        });
+        return clonedSet as any as T;
+    }
+
+    if (v instanceof Map) {
+        const clonedMap = new Map<any, any>();
+        v.forEach((value, key) => {
+            clonedMap.set(deepClone(key), deepClone(value));
+        });
+        return clonedMap as any as T;
+    }
+
+    const clone: any = isArray(v) ? [] : {};
+
+    for (const key in v) {
+        if (hasOwn.call(v, key)) {
+            clone[key] = deepClone(v[key]);
         }
     }
-    return result;
+
+    return clone as T;
 }
