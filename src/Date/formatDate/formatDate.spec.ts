1503471cbee90a96fcd3e97c796e677241717edc
Hyhello <673089899@qq.com>
2024-05-23 13:11:49 +0800

    Update formatDate.spec.ts

--- src/Date/formatDate/formatDate.spec.ts
+++ src/Date/formatDate/formatDate.spec.ts
@@ -6,6 +6,7 @@ describe('#formatDate()', () => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         const d = new Date(1639380703857);
+        const hour = padZero(d.getHours());
         const str =
             d.getFullYear() +
             '-' +
@@ -13,7 +14,7 @@ describe('#formatDate()', () => {
             '-' +
             d.getDate() +
             ' ' +
-            padZero(d.getHours()) +
+            hour +
             ':' +
             d.getMinutes() +
             ':' +
@@ -22,8 +23,8 @@ describe('#formatDate()', () => {
         // @ts-ignore
         expect(formatDate()).toEqual('Invalid Date');
         expect(formatDate(1639380703857)).toEqual(str);
-        expect(formatDate(1639380703857, 'hh:mm:SS')).toEqual('15:31:857');
-        expect(formatDate(1639380703857 - 5 * 60 * 60 * 1000, 'h:m:S')).toEqual('10:31:857');
+        expect(formatDate(1639380703857, 'hh:mm:SS')).toEqual(`${hour}:31:857`);
+        expect(formatDate(1639380703857 - 5 * 60 * 60 * 1000, 'h:m:S')).toEqual(`${d.getHours()}:31:857`);
         expect(formatDate('2021-12-13', 'yyyy-MM-dd')).toEqual('2021-12-13');
     });
 });
