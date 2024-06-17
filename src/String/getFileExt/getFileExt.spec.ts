6d2d44c3d556621f6e3acb6fa9f24af83339bb91
Hyhello <673089899@qq.com>
2024-06-17 11:08:09 +0800

    docs: 优化文档

--- src/String/getFileExt/getFileExt.spec.ts
+++ src/String/getFileExt/getFileExt.spec.ts
@@ -4,6 +4,7 @@ describe('#getFileExt()', () => {
     test('getFileExt test', () => {
         expect(getFileExt('aaa.png')).toEqual('png');
         expect(getFileExt('aaa.png.a')).toEqual('a');
+        expect(getFileExt('aaa')).toEqual('');
         expect(getFileExt('aaa.png.m') === 'png.m').toBeFalsy();
     });
 });
