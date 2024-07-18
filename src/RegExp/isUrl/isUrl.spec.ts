4a4222e08cbcfc04953b12bcd6d19c04389d98fa
Hyhello <673089899@qq.com>
2024-07-18 21:07:49 +0800

    fix: Fix the issue of isURL matching `/a/b.png` that can pass

--- src/RegExp/isUrl/isUrl.spec.ts
+++ src/RegExp/isUrl/isUrl.spec.ts
@@ -3,7 +3,11 @@ import isUrl from './isUrl';
 describe('#isUrl', () => {
     test('isUrl', () => {
         expect(isUrl('你好')).toBeFalsy();
+        expect(isUrl('/a/b/c.png')).toBeFalsy();
         expect(isUrl('www.42@qq.com')).toBeFalsy();
+        expect(isUrl('http://192.168.1.1:8090')).toBeTruthy();
+        expect(isUrl('http://192.168.1.1')).toBeTruthy();
+        expect(isUrl('http://www.baidu.com:8088')).toBeTruthy();
         expect(isUrl('http://www.baidu.com')).toBeTruthy();
     });
 });
