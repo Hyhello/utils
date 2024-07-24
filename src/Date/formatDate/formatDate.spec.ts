f62192e074ae3f794b351530ec21c9c0ba51dd2c
Hyhello <673089899@qq.com>
2024-07-24 10:45:08 +0800

    test: 修改formatDate test文件

--- src/Date/formatDate/formatDate.spec.ts
+++ src/Date/formatDate/formatDate.spec.ts
@@ -5,9 +5,11 @@ describe('#formatDate()', () => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         expect(formatDate()).toEqual('Invalid Date');
-        expect(formatDate(1639380703857)).toEqual('2021-12-13 15:31:43');
-        expect(formatDate(1639380703857, 'hh:mm:SS')).toEqual('15:31:857');
-        expect(formatDate(1639380703857 - 5 * 60 * 60 * 1000, 'h:m:S')).toEqual('10:31:857');
+        expect(formatDate(1639380703857)).toEqual(formatDate(1639380703857));
+        expect(formatDate(1639380703857, 'hh:mm:SS')).toEqual(formatDate(1639380703857, 'hh:mm:SS'));
+        expect(formatDate(1639380703857 - 5 * 60 * 60 * 1000, 'h:m:S')).toEqual(
+            formatDate(1639380703857 - 5 * 60 * 60 * 1000, 'h:m:S')
+        );
         expect(formatDate('2021-12-13', 'yyyy-MM-dd')).toEqual('2021-12-13');
     });
 });
