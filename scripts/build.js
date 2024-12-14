3a665e4c205824ffee35095d412b0055cc69e9ea
Hyhello <673089899@qq.com>
2024-12-14 13:39:08 +0800

    feat: compare 方法添加order 参数

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs-extra');
 const rollup = require('rollup');
 const config = require('./config');
@@ -6,7 +5,7 @@ const { del } = require('./utils');
 const genTypes = require('./genTypes');
 const components = require('./components');
 const packageJson = require('../package.json');
-const { terser } = require('rollup-plugin-terser');
+const terser = require('@rollup/plugin-terser');
 const filesize = require('rollup-plugin-filesize');
 const commonjs = require('@rollup/plugin-commonjs');
 const typescript = require('rollup-plugin-typescript2');
@@ -17,10 +16,10 @@ const author = packageJson.author || 'Hyhello';
 const version = process.env.VERSION || packageJson.version;
 
 const banner =
-	'/*!\n' +
-	` * ${packageJson.name} v${version}\n` +
-	` * (c) 2021-present ${author}\n` +
-	' * Released under the MIT License.\n' +
+    '/*!\n' +
+    ` * ${packageJson.name} v${version}\n` +
+    ` * (c) 2021-present ${author}\n` +
+    ' * Released under the MIT License.\n' +
     ' */';
 
 const terserPlugin = terser({
@@ -44,25 +43,24 @@ const rollupConfig = {
 
 // 构建componets
 const buildComponents = async () => {
-    const componentsList = Object.keys(components);
-    for (let name of componentsList) {
-        const bundle = await rollup.rollup({
-            ...rollupConfig,
-            input: components[name]
-        });
-        bundle.write({
-            format: 'esm',
-            name,
-            file: `${config.outputDir}/${name}.js`
-        });
-    }
-}
+    const bundle = await rollup.rollup({
+        ...rollupConfig,
+        input: components
+    });
+    await bundle.write({
+        format: 'esm',
+        dir: 'lib',
+        generatedCode: 'es2015'
+    });
+};
 
 // 构建所有
 async function buildEntry() {
     const { pkgName, outputDir, outputTypeList } = config;
     del([outputDir], false);
+
     await buildComponents();
+
     const bundle = await rollup.rollup(rollupConfig);
     for (let i in outputTypeList) {
         const format = outputTypeList[i];
--- src/Array/compare/compare.ts
+++ src/Array/compare/compare.ts
@@ -1,6 +1,9 @@
 import { isObject, isUndefined } from '@/Base';
 
-type ArgType = string | number | { [propName: string]: any };
+const enum ORDER {
+    ASC = 'asc',
+    DESC = 'desc'
+}
 
 /**
  * 服务于sort，用于排序
@@ -8,13 +11,14 @@ type ArgType = string | number | { [propName: string]: any };
  * @returns {Function} 返回排序后的数组。
  * @see {@link https://hyhello.github.io/utils/#/compare 在线文档}
  */
-export default function compare(key?: string | number): (arg1: ArgType, arg2: ArgType) => number {
+export default function compare(key?: string | number, order?: 'asc' | 'desc'): <T>(arg1: T, arg2: T) => number {
     const hasKey = !isUndefined(key);
-    return function (arg1: ArgType, arg2: ArgType): number {
-        const target1 = hasKey && isObject(arg1) ? arg1[<keyof ArgType>key] : arg1;
-        const target2 = hasKey && isObject(arg2) ? arg2[<keyof ArgType>key] : arg2;
-        if (target1 > target2) return 1;
-        if (target1 < target2) return -1;
+    order = order || ORDER.ASC;
+    return function <T>(arg1: T, arg2: T): number {
+        const target1 = hasKey && isObject(arg1) ? arg1[key as keyof T] : arg1;
+        const target2 = hasKey && isObject(arg2) ? arg2[key as keyof T] : arg2;
+        if (target1 > target2) return order === ORDER.ASC ? 1 : -1;
+        if (target1 < target2) return order === ORDER.ASC ? -1 : 1;
         return 0;
     };
 }
